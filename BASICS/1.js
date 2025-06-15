b31167f174e85aa6221c42f375944577ad240fec
Madhur <madhurbiradar21@gmail.com>
2025-06-15 23:46:39 +0530

    data-types

---
+++ BASICS/1.js
@@ -0,0 +1,8 @@
+const name = "madhur" //assigned value can never be chnaged
+var age =18  //global variable {not prefferable}
+let age2 = 22 //scope-based {preffered}
+a=55 //global declaration {never use}
+let hobby; //if we don't store anything then Undefined will be output
+//const requires initialization at the time of declaration
+
+console.log(age);
