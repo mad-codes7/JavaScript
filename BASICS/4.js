f441a9186a9f4a31c9b18e1e548ed4bbbc67e7a8
Madhur <madhurbiradar21@gmail.com>
2025-06-16 21:09:13 +0530

    operator and pre&post

---
+++ BASICS/4.js
@@ -0,0 +1,14 @@
+//operations
+
+ 
+// console.log(7/2);
+// //console.log(7//2); => floor division is not available in JS
+// console.log(Math.floor(7/2))
+// console.log(7%2)
+
+str3 = "madhur"+" biradar"
+console.log(str3);
+console.log(22+22);
+console.log("22 " + 22); // =>converts num to string forcefully, when there is one string already available
+console.log(22+"22");
+
