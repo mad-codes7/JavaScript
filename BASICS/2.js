d12b70ffbcddfaf14a7566b6a91ba5695a521f9a
Madhur <madhurbiradar21@gmail.com>
2025-06-16 12:45:45 +0530

    data-types in detail

---
+++ BASICS/2.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version, but now it is automatically implemented...
+alert(22) // will not run in terminal{node}, only works in browser
+
+//primitive data types
+/*
+1 string
+2 number 
+3 boolean
+4 bigint 
+5 symbol
+6 undefined
+7 null
+*/
+
+//secondary data types
+/* 
+1 object
+2 array
+3 function
+*/
+
+//console.log(typeof undefined) =>  undefined
+//console.log(typeof null) => null
