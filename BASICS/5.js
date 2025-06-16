0cf47d1845e62901831f26d8512bac88b4279c46
Madhur <madhurbiradar21@gmail.com>
2025-06-16 21:48:15 +0530

    comparison operator  and special cases of null&undefined

---
+++ BASICS/5.js
@@ -0,0 +1,16 @@
+//comparison operators
+//comparison is easy in same data-type 
+//but ambiguity arises when there are two different data types!
+// console.log(2>=1);
+// console.log("2">="5");
+
+
+// console.log(null>0) //f
+// console.log(null==0) //f
+// console.log(null>=0) //t
+
+//strict_check ===
+
+
+console.log("2"==2);
+console.log("2"===2);
